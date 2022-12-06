5dd0d4bc9cfc52ac915fa3258992952582dd4b6e
ajay.prasanna <ajay.prasanna@arriver.com>
2022-12-06 12:37:57 +0100

    2022 day4 part 2 solved!!

--- 2022/puzzle4.py
+++ 2022/puzzle4.py
@@ -24,16 +24,10 @@ def how_many_partially_contained(input):
         x, y = item.split(',')
         x1, y1 = x.split('-')
         x2, y2 = y.split('-')
-        if x2 >= x1 <= y1 and y2 <= y1 >= x1:
-            count += 1
-        elif x1 >= x2 <= y2 and y1 <= y2 >= x2:
-            count += 1
-        elif (((x2 >= x1) and (x2 <= y1)) or ((y2 <= y1) and (y2 >= x1))):
-        #if x2 >= x1 <= y1 or y2 <= y1 >= x1:
-            count += 1
-        elif (((x1 >= x2) and (x1 <= y2)) or ((y1 <= y2) and (y1 >= x2))):
-            count += 1
-    return count
+        # using negative condition: check if pairs do not overlap
+        if int(y1)<int(x2) or int(y2)<int(x1):
+            count+=1
+    return len(input) - count # remaining ones does overlap partially/fully
     
 
 if __name__ == '__main__':
